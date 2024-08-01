de4b2e71e772349d865338df2b4fb7e985ea8964
severous <9036233+severous@user.noreply.gitee.com>
2024-08-01 08:48:15 +0800

    feat:classloader

--- async-log-client/src/main/java/module-info.java
+++ async-log-client/src/main/java/module-info.java
@@ -1,10 +1,10 @@
 import javax.annotation.processing.Processor;
-import org.example.async.log.client.annotation.AsyncLogProcessor;
+import org.example.async.log.client.annotation.AsyncLogAnnotationProcessor;
 
 module org.example.async.log.client{
     requires jdk.compiler;
     requires jdk.unsupported;
 
     exports org.example.async.log.client.annotation;
-    provides Processor with AsyncLogProcessor;
+    provides Processor with AsyncLogAnnotationProcessor;
 }
